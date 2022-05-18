fa6fe458b6f598dc9839a723c0f1e38616df1c5c
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-05-18 16:53:15 +0300

    layout completed added single page and 404 page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,59 @@
 $(document).ready(function() {
 
+  $('.scrollto a').on('click', function scroll(e) {
+		e.preventDefault();
+		let href = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(href).offset().top
+		}, {
+			duration: 370,
+			easing: "linear"
+		});
+		return false;
+	});
+  
+  /* Popup form*/
+
+$('.similar__box__items__btn').click(function (e) {
+    e.preventDefault();
+    let price = $(this).siblings(".similar__box__items__price").text();
+    let titleproduct = $(this).siblings(".similar__box__items__none").text();
+    $("#modaltitle").val(titleproduct);
+    $("#modalprice").val(price);
+    $( ".orderoneclick" ).addClass('open');
+});
+$('.box__sidebar__btn').click(function (e) {
+    e.preventDefault();
+    let title = $(this).siblings(".box__sidebar__title").text();
+    $("input:hidden").val(title);
+    $( ".consultation" ).addClass('open');
+});
+
+$('.cart__desc__btn').click(function (e) {
+  e.preventDefault();
+  $( ".productoneclick" ).addClass('open');
+});
+
+$('.rating__video--one').click(function (e) {
+    e.preventDefault();
+    $( ".video--one" ).addClass('open');
+});
+
+$('.rating__video--two').click(function (e) {
+  e.preventDefault();
+  $( ".video--two" ).addClass('open');
+});
+
+$('.popup__header_close').click(function (e) {
+    e.preventDefault();
+    $( ".popup" ).removeClass('open');
+});
+
+$('.popup__area').click(function (e) {
+  e.preventDefault();
+  $( ".popup" ).removeClass('open');
+});
+
   $('.cart__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
